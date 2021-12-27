cf51424b0714c865837a98aeadd84394ad1ae7d9
losuler <losuler@posteo.net>
2021-12-28 06:41:00 +1100

    Add variables for long URLs for better readability

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,12 @@ ICECATCOMMIT="32631cac00953abbac61dc7ab1a0eafbdd59b53a"
 FFVERSION="91.3.0"
 CLVERSION="RELEASE_8_1_0"
 
+MOZ_FTP="ftp.mozilla.org/pub/firefox/releases"
+MOZ_SOURCE="${MOZ_FTP}/${FFVERSION}esr/source"
+
+MOZ_HG="hg.mozilla.org"
+GNU_GIT="git.savannah.gnu.org/cgit/gnuzilla.git/snapshot"
+
 usage() {
     echo "Usage: build.sh COMMAND"
     echo "Commands:"
@@ -19,27 +25,27 @@ usage() {
 }
 
 download() {
-    wget "https://git.savannah.gnu.org/cgit/gnuzilla.git/snapshot/gnuzilla-${ICECATCOMMIT}.tar.gz"
+    wget "https://${GNU_GIT}/gnuzilla-${ICECATCOMMIT}.tar.gz"
     mv gnuzilla-${ICECATCOMMIT}.tar.gz icecat_${FFVERSION}.orig.tar.gz
 
     mkdir -p output
     cd output
 
-    wget "https://ftp.mozilla.org/pub/firefox/releases/${FFVERSION}esr/source/firefox-${FFVERSION}esr.source.tar.xz"
-    wget "https://ftp.mozilla.org/pub/firefox/releases/${FFVERSION}esr/source/firefox-${FFVERSION}esr.source.tar.xz.asc"
-    wget "https://ftp.mozilla.org/pub/firefox/releases/${FFVERSION}esr/KEY"
+    wget "https://${MOZ_FTP}/${FFVERSION}esr/source/firefox-${FFVERSION}esr.source.tar.xz"
+    wget "https://${MOZ_FTP}/${FFVERSION}esr/source/firefox-${FFVERSION}esr.source.tar.xz.asc"
+    wget "https://${MOZ_FTP}/${FFVERSION}esr/KEY"
 
     tar xf "firefox-${FFVERSION}esr.source.tar.xz" --checkpoint=.1000
 
     while read line; do
         line=$(echo $line | cut -d ' ' -f1)
         [ $line = "en-US" ] && continue
-        wget --content-disposition "https://hg.mozilla.org/l10n-central/$line/archive/tip.zip"
+        wget --content-disposition "https://${MOZ_HG}/l10n-central/$line/archive/tip.zip"
     done < "firefox-$FFVERSION/browser/locales/shipped-locales"
 
     rm -r firefox-$FFVERSION
 
-    wget --content-disposition "https://hg.mozilla.org/l10n/compare-locales/archive/$CLVERSION.zip"
+    wget --content-disposition "https://${MOZ_HG}/l10n/compare-locales/archive/$CLVERSION.zip"
 
     cd ..
     tar caf "icecat_$FFVERSION.orig-output.tar.xz" --checkpoint=.1000 output/*
