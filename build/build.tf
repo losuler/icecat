5174bca106a5afa49cb94ebd599af13e56e31d1a
losuler <losuler@posteo.net>
2022-06-12 02:44:03 +1000

    feat: add test build deployment

---
+++ build/build.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.18"
+    }
+  }
+
+  required_version = ">= 1.2.2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "icecat_build" {
+  ami           = "ami-08b7bda26f4071b80"
+  instance_type = "m5.4xlarge"
+  key_name      = "icecat-build-aws"
+
+  root_block_device {
+    volume_size = "40"
+  }
+
+  tags = {
+    Name = "icecat-build"
+  }
+}
