efc66a4cd6dcccab30ea413f81c2d91628ad575a
losuler <losuler@posteo.net>
2022-06-26 23:49:06 +1000

    fix: shellcheck SC2162 read without -r will mangle backslashes

--- build/build.sh
+++ build/build.sh
@@ -47,7 +47,7 @@ download() {
 
     tar xf "firefox-${FFVERSION}esr.source.tar.xz" --checkpoint=.1000
 
-    while read line; do
+    while read -r line; do
         line=$(echo $line | cut -d ' ' -f1)
         [ $line = "en-US" ] && continue
         wget --output-document $line.zip "https://${MOZ_HG}/l10n-central/$line/archive/tip.zip"
@@ -75,13 +75,13 @@ create_service() {
     wget "https://${MOZ_FTP}/${FFVERSION}esr/source/firefox-${FFVERSION}esr.source.tar.xz"
     tar xf "firefox-${FFVERSION}esr.source.tar.xz" --checkpoint=.1000
 
-    while read line; do
+    while read -r line; do
         line=$(echo $line | cut -d ' ' -f1)
         [ $line = "en-US" ] && continue
         osc add "https://${MOZ_HG}/l10n-central/$line/archive/tip.zip" || true
     done < "firefox-$FFVERSION/browser/locales/shipped-locales"
 
-    while read line; do
+    while read -r line; do
         if [[ $line == $(echo $line | grep l10n-central) ]]; then
             lang=$(echo $line | awk -F '/' '{print $3}')
             sed -i -e "s|$line|$line\n    <param name=\"filename\">$lang.zip</param>|" _service
